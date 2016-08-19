34d8d7a70038550eecc1befd75a112fd8ab56a0f
Diogo Wernik <diogowernik@gmail.com>
2016-08-18 21:59:51 -0400

    new changes

--- demo/www/js/app.js
+++ demo/www/js/app.js
@@ -39,7 +39,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'io
                 controller: 'ActivityCtrl'
             },
             'fabContent': {
-                template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 flap"><i class="icon ion-paper-airplane"></i></button>',
+                template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
                 controller: function ($timeout) {
                     $timeout(function () {
                         document.getElementById('fab-activity').classList.toggle('on');
